b759f87e033a46cb02f9105a435564853e268b4b
user2 <florian.martin63000@gmail.com>
2020-11-30 13:31:23 +0100

    Feat: add joke resources

--- src/main/java/com/gitlabci/kubernetes/controller/RootController.java
+++ src/main/java/com/gitlabci/kubernetes/controller/RootController.java
@@ -20,4 +20,9 @@ public class RootController {
     public String kida() {
         return "Envoi une photo de Kida, bah répond";
     }
+	
+    @GetMapping("/joeystarr")
+    public String joeyStarr() {
+	return "'Ludo je le calcule pas c'est une pompe à vélo.' J. Starr - 2012";
+    }
 }
